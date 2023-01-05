e486736e24d3deaca5f3e31c730a5b692da5dffa
Shatakshi Mondal <54851148+ShatakshiMondal@users.noreply.github.com>
2023-01-05 16:17:36 +0530

    Update streamlit_app.py
    commented the lines

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,9 +11,9 @@ streamlit.text('🥑🍞 Avacado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 import pandas
-''' We want pandas to read our CSV file from that S3 bucket so we use a pandas function 
-called read_csv  to pull the data into a dataframe we'll call my_fruit_list. '''
+#We want pandas to read our CSV file from that S3 bucket so we use a pandas function 
+#called read_csv  to pull the data into a dataframe we'll call my_fruit_list.
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-'''After pulling the data into a pandas dataframe called my_fruit_list, 
-we will ask the streamlit library to display it on the page by typing:'''
+#After pulling the data into a pandas dataframe called my_fruit_list, 
+#we will ask the streamlit library to display it on the page by typing:
 streamlit.dataframe(my_fruit_list)
