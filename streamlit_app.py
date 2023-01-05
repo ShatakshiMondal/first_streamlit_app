c7e3038de49df71dc6707ea514910c7cecc9cd8b
Shatakshi Mondal <54851148+ShatakshiMondal@users.noreply.github.com>
2023-01-05 16:16:35 +0530

    Update streamlit_app.py
    rectified the error

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,4 +16,4 @@ called read_csv  to pull the data into a dataframe we'll call my_fruit_list. '''
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 '''After pulling the data into a pandas dataframe called my_fruit_list, 
 we will ask the streamlit library to display it on the page by typing:'''
-streamlit_dataframe(my_fruit_list)
+streamlit.dataframe(my_fruit_list)
